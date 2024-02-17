bc7c285985e6c1e80cdf5bc6baaccf8d36cecc5b
victor montebello <montebellovictor@gmail.com>
2024-02-17 11:30:00 -0300

    feat: servidor de rotas retornando todas as rotas

--- api/src/router/rotas/rotas.go
+++ api/src/router/rotas/rotas.go
@@ -2,6 +2,7 @@ package rotas
 
 import (
 	"net/http"
+	"github.com/gorilla/mux"
 )
 
 // Rota : Representa todas as rotas da aplicação
@@ -13,3 +14,10 @@ type Rota struct {
 }
 
 // Configurar : Inicializa as rotas da aplicação
+func Configurar(r *mux.Router) *mux.Router {
+	rotas := rotasUsuarios
+	for _, rota := range rotas {
+		r.HandleFunc(rota.URI, rota.Funcao).Methods(rota.Metodo)
+	} 
+	return r
+}
--- api/src/router/router.go
+++ api/src/router/router.go
@@ -1,10 +1,13 @@
 package router
 
 import (
+	"modulo/src/router/rotas"
+
 	"github.com/gorilla/mux"
 )
 
 // Gerar : Retorna um roteador com as rotas configuradas
 func Gerar() *mux.Router{
-	return mux.NewRouter()
+	r := mux.NewRouter()
+	return rotas.Configurar(r)
 }
