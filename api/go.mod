acbe59fde37e0287d26b370614aa4209a1c35942
victor montebello <montebellovictor@gmail.com>
2024-02-17 15:22:47 -0300

    configurando variaveis de ambiente

--- api/go.mod
+++ api/go.mod
@@ -3,3 +3,5 @@ module modulo
 go 1.21.5
 
 require github.com/gorilla/mux v1.8.1
+
+require github.com/joho/godotenv v1.5.1
--- api/main.go
+++ api/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"modulo/src/config"
 	"modulo/src/router"
 	"net/http"
 )
 
 func main() {
+	config.Carregar()
+	fmt.Println(config.StringConexaoBanco)
 	r := router.Gerar()
-	log.Fatal(http.ListenAndServe(":8080", r))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Porta), r))
 }
